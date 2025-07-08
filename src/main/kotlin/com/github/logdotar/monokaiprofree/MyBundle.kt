3d1aae5b126de9cbf1daff4811645422053f95bb
GitHub Action <action@github.com>
2025-07-08 07:12:27 +0000

    Template cleanup

--- src/main/kotlin/com/github/logdotar/monokaiprofree/MyBundle.kt
+++ src/main/kotlin/com/github/logdotar/monokaiprofree/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.logdotar.monokaiprofree
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/logdotar/monokaiprofree/services/MyProjectService.kt
+++ src/main/kotlin/com/github/logdotar/monokaiprofree/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.logdotar.monokaiprofree.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.logdotar.monokaiprofree.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
